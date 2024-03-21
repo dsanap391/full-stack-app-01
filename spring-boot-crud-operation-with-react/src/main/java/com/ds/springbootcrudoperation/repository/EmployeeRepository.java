ae3127a35bbf26d994c6304c0b5aaf2797ea6dcc
dattatrays29 <dattatrays1207@gmail.com>
2024-03-22 01:51:48 +0530

    added @enableEncryptableProperties annotation

--- spring-boot-crud-operation-with-react/src/main/java/com/ds/springbootcrudoperation/repository/EmployeeRepository.java
+++ spring-boot-crud-operation-with-react/src/main/java/com/ds/springbootcrudoperation/repository/EmployeeRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ds.springbootcrudoperation.model.Employee;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @Repository
+@EnableEncryptableProperties
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
